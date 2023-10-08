35f72c81b3bc849bc42b2eb24b018f1fe03f434e
Yaroslav Klimas <yarklim@gmail.com>
2023-10-08 15:50:45 +0300

    created navigation dots

--- src/components/SimpleSlider/SimpleSlider.jsx
+++ src/components/SimpleSlider/SimpleSlider.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import s from './SimpleSlider.module.scss';
+// Icons for Dots style
+// import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+// import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 
 import { imagesData } from '../../data/imagesData';
 
@@ -24,6 +27,7 @@ const SimpleSlider = () => {
 
   return (
     <section className={s.sliderContainer}>
+      {/* Images */}
       <div className={s.imagesWrapper}>
         {imagesData?.map(({ id, image, alt }) => (
           <img
@@ -35,21 +39,47 @@ const SimpleSlider = () => {
           />
         ))}
       </div>
-      {/* <img className={s.imgSlider} src={imagesData[imgIndex].image} alt="pic" /> */}
+
+      {/* Navigations Arrows */}
       <button
-        className={s.sliderArrowBtn}
+        className={s.navigateArrowBtn}
         style={{ left: 0 }}
         onClick={showPrevImage}
       >
         <ArrowBackIosNewIcon />
       </button>
       <button
-        className={s.sliderArrowBtn}
+        className={s.navigateArrowBtn}
         style={{ right: 0 }}
         onClick={showNextImage}
       >
         <ArrowForwardIosIcon />
       </button>
+
+      {/* Navigation Dots */}
+      <div className={s.navigateDotsWrapper}>
+        {imagesData?.map((_, index) => (
+          <button
+            className={s.navigateDot}
+            key={index}
+            onClick={() => setImgIndex(index)}
+          >
+            {/* Dots style*/}
+            {/* {index === imgIndex ? (
+              <RadioButtonCheckedIcon />
+            ) : (
+              <RadioButtonUncheckedIcon />
+            )} */}
+
+            {/* Lines style */}
+            {index === imgIndex ? (
+              <div className={s.dotChecked}></div>
+            ) : (
+              <div className={s.dotUnchecked}></div>
+            )}
+          </button>
+        ))}
+      </div>
     </section>
   );
 };
