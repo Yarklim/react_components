e89ee2dffdb25f1f1b5901fadcfda43c4c5ce288
Yaroslav Klimas <yarklim@gmail.com>
2023-10-17 17:59:05 +0300

    created useDebounce

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,18 @@
+import { useCallback, useRef } from 'react';
+
+const useDebounce = (callback, delay) => {
+  const timer = useRef();
+
+  const debouncedCallback = useCallback(() => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+    timer.current = setTimeout(() => {
+      callback();
+    }, delay);
+  }, [callback, delay]);
+
+  return debouncedCallback;
+};
+
+export default useDebounce;
